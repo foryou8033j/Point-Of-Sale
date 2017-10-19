ef2ba40aada05a671b30fda963954b0c406373e1
Jeongsam <foryou8033j@gmail.com>
2017-10-19 23:49:26 +0900

    카드사 데이터 추가

--- Point-Of-Sale/src/PointOfView/Models/Receipt/Receipt.java
+++ Point-Of-Sale/src/PointOfView/Models/Receipt/Receipt.java
@@ -1,5 +1,10 @@
 package PointOfView.Models.Receipt;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import PointOfView.Models.Table.TableData;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -10,6 +15,52 @@ import javafx.collections.ObservableList;
  *
  */
 public class Receipt {
+	
+	//카드사 정보를 구현할 InnerClass, Static으로 선언한다
+	public static class CardCompany {
+		
+		private static Map<String, String> cardCompany = new HashMap<String, String>(); 
+		
+		static
+	    {
+			cardCompany.put("KB국민카드", "KB국민카드체크");
+			cardCompany.put("KB국민카드", "KB국민카드신용");
+			cardCompany.put("신한카드", "KB신한카드체크");
+			cardCompany.put("신한카드", "KB신한카드신용");
+			cardCompany.put("NH농협", "NH농협체크");
+			cardCompany.put("NH농협", "NH농협신용");
+	    }
+		
+		/**
+		 * 카드사를 반환한다
+		 * @param i 인덱스
+		 * @return 카드사명
+		 */
+		public static String getCompany(int i) {
+			List keys = new ArrayList(cardCompany.keySet());
+			return (String)keys.get(i);
+		}
+		
+		/**
+		 * 카드 이름을 반환한다.
+		 * @param name 카드사
+		 * @return 카드 이름
+		 */
+		public static String getCardName(String name) {
+			return cardCompany.get(name);
+		}
+		
+		/**
+		 * Map 사이즈를 반환한다.
+		 * @return int
+		 */
+		public static int getSize() {
+			return cardCompany.size();
+		}
+		
+		
+		
+	}
 	
 	private ObservableList<ReceiptModel> receiptList;
 	
@@ -31,6 +82,8 @@ public class Receipt {
 	 * @param tableData
 	 */
 	public void addReceiptList(PAY_WAY payWay, TableData tableData) {
-		receiptList.add(new ReceiptModel(payWay, tableData));
+		
+		int index = receiptList.size();
+		receiptList.add(new ReceiptModel(payWay, tableData, index));
 	}
 }
--- Point-Of-Sale/src/PointOfView/Models/Receipt/ReceiptModel.java
+++ Point-Of-Sale/src/PointOfView/Models/Receipt/ReceiptModel.java
@@ -2,8 +2,12 @@ package PointOfView.Models.Receipt;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
+import java.util.Random;
 
+import PointOfView.Models.Receipt.Receipt.CardCompany;
 import PointOfView.Models.Table.TableData;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
@@ -23,7 +27,15 @@ public class ReceiptModel {
 	private StringProperty pay = new SimpleStringProperty();
 	private StringProperty money = new SimpleStringProperty();
 	
-	public ReceiptModel(PAY_WAY payWay, TableData tableData) {
+	private int tradeIndex;
+	private String cardCompany = "";
+	private String cardName = "";
+	private String cardNumber = "";
+	
+	public ReceiptModel(PAY_WAY payWay, TableData tableData, int index) {
+		
+		//결제 영수증 고유 번호 저장
+		tradeIndex = index;
 		
 		//결제 시간 저장
 		this.payTime = Calendar.getInstance();
@@ -35,13 +47,27 @@ public class ReceiptModel {
 		this.tableData.copyData(tableData);
 		
 		
-		
-		SimpleDateFormat format = new SimpleDateFormat("yyyy.MM.dd(E)  HH:mm:ss:SSS", Locale.KOREA );
+		SimpleDateFormat format = new SimpleDateFormat("yyyy.MM.dd(E)  HH:mm:ss", Locale.KOREA );
 		date.set(format.format(payTime.getTimeInMillis()));
 		
 		pay.set(payWay.toString());
 		
 		money.set(String.valueOf(tableData.getSumPrice()));
+		
+		
+		//카드 결제가 아니면 무시
+		if(!payWay.equals(PAY_WAY.CARD)) 
+			return;
+		
+		//결제를 구현하기 위해 가상으로 카드사나 기타 정보를 저장
+		this.cardCompany = CardCompany.getCompany(new Random().nextInt(CardCompany.getSize()));
+		this.cardName = CardCompany.getCardName(cardCompany);
+		
+		int rnd0 = new Random().nextInt((9999 - 1000) + 1) + 1000;
+		int rnd1 = new Random().nextInt((9999 - 1000) + 1) + 1000;
+		int rnd2 = new Random().nextInt((9999 - 1000) + 1) + 1000;
+		
+		cardNumber = String.valueOf(rnd0) + "-" + String.valueOf(rnd1) + "-****-" + String.valueOf(rnd2);
 		
 	}
 	
@@ -57,6 +83,9 @@ public class ReceiptModel {
 		return money;
 	}
 	
+	public int getIndex() {
+		return tradeIndex;
+	}
 	
 	public PAY_WAY getPayWay() {
 		return payWay;
@@ -68,6 +97,18 @@ public class ReceiptModel {
 	
 	public TableData getPayTableData() {
 		return tableData;
+	}
+	
+	public String getCardCompany() {
+		return cardCompany;
+	}
+	
+	public String getCardName() {
+		return cardName;
+	}
+	
+	public String getCardNumber() {
+		return cardNumber;
 	}
 	
 }
