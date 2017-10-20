664ecca8d3557d5fc51caf2ae41a78e23188494f
Jeongsam <foryou8033j@gmail.com>
2017-10-20 17:48:29 +0900

    매출 통계 부분 추가

--- Point-Of-Sale/src/PointOfView/View/Stastics/Sale/SaleStasticsLayoutController.java
+++ Point-Of-Sale/src/PointOfView/View/Stastics/Sale/SaleStasticsLayoutController.java
@@ -1,5 +1,41 @@
 package PointOfView.View.Stastics.Sale;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+
 public class SaleStasticsLayoutController {
+
+    @FXML
+    private BarChart<?, ?> yearChart;
+
+    @FXML
+    private BarChart<?, ?> monthChart;
+
+    @FXML
+    private BarChart<?, ?> weekChart;
+
+    @FXML
+    private BarChart<?, ?> dayChart;
+
+    @FXML
+    void dayChartOpen(ActionEvent event) {
+
+    }
+
+    @FXML
+    void monthChartOpen(ActionEvent event) {
+
+    }
+
+    @FXML
+    void weekChartOpen(ActionEvent event) {
+
+    }
+
+    @FXML
+    void yearChartOpen(ActionEvent event) {
+
+    }
 
 }
--- Point-Of-Sale/src/PointOfView/View/Stastics/StasticsLayoutController.java
+++ Point-Of-Sale/src/PointOfView/View/Stastics/StasticsLayoutController.java
@@ -1,9 +1,14 @@
 package PointOfView.View.Stastics;
 
 import PointOfView.MainApp;
+import PointOfView.View.Stastics.Menu.MenuStasticsLayoutController;
+import PointOfView.View.Stastics.Sale.SaleStasticsLayoutController;
+import PointOfView.View.Stastics.Time.TimeStasticsLayoutController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Tab;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class StasticsLayoutController {
@@ -23,18 +28,45 @@ public class StasticsLayoutController {
     }
 
     @FXML
-    void handleMenuTabSelected(ActionEvent event) {
-
-    }
-
-    @FXML
     void handleSaleTabSelected(ActionEvent event) {
-
+    	try {
+    		FXMLLoader loader = new FXMLLoader(getClass().getResource("Sale/SaleStasticsLayout.fxml"));
+    		BorderPane pane = loader.load();
+    		
+    		SaleStasticsLayoutController controller = loader.getController();
+    		
+    		saleStasticsTab.setContent(pane);
+    	}catch (Exception e) {
+    		//ignore
+    	}
+    }
+    
+    @FXML
+    void handleMenuTabSelected(ActionEvent event) {
+    	try {
+    		FXMLLoader loader = new FXMLLoader(getClass().getResource("Menu/MenuStasticsLayout.fxml"));
+    		BorderPane pane = loader.load();
+    		
+    		MenuStasticsLayoutController controller = loader.getController();
+    		
+    		menuStasticsTab.setContent(pane);
+    	}catch (Exception e) {
+    		//ignore
+    	}
     }
 
     @FXML
     void handleTimeTabSelected(ActionEvent event) {
-
+    	try {
+    		FXMLLoader loader = new FXMLLoader(getClass().getResource("Time/TimeStasticsLayout.fxml"));
+    		BorderPane pane = loader.load();
+    		
+    		TimeStasticsLayoutController controller = loader.getController();
+    		
+    		timeStasticsPane.setContent(pane);
+    	}catch (Exception e) {
+    		//ignore
+    	}
     }
     
     private MainApp mainApp;
@@ -43,6 +75,12 @@ public class StasticsLayoutController {
     public void setMainApp(Stage stage, MainApp mainApp) {
     	this.mainApp = mainApp;
     	this.stage = stage;
+    	
+    	handleSaleTabSelected(null);
+    	handleTimeTabSelected(null);
+    	handleMenuTabSelected(null);
+    	
+    	
     }
 
 }
