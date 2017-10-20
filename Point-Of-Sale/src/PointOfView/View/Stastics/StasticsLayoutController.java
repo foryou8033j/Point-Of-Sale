971fd37deb020ce2309490521c59e6e01b19c58f
Jeongsam <foryou8033j@gmail.com>
2017-10-20 17:09:32 +0900

    통계 레이아웃 추가

---
+++ Point-Of-Sale/src/PointOfView/View/Stastics/StasticsLayoutController.java
@@ -0,0 +1,22 @@
+package PointOfView.View.Stastics;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Tab;
+
+public class StasticsLayoutController {
+
+    @FXML
+    private Tab saleStasticsTab;
+
+    @FXML
+    private Tab menuStasticsTab;
+
+    @FXML
+    private Tab timeStasticsPane;
+
+    @FXML
+    void tabSelected(ActionEvent event) {
+
+    }
+
+}
