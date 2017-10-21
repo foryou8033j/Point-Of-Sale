558d2745394dd1102384d27c330b895cbd98c5fc
Jeongsam <foryou8033j@gmail.com>
2017-10-21 18:51:43 +0900

    소스 정리

--- Point-Of-Sale/src/PointOfView/View/RootLayoutController.java
+++ Point-Of-Sale/src/PointOfView/View/RootLayoutController.java
@@ -12,80 +12,80 @@ import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 
-public class RootLayoutController implements Initializable{
+public class RootLayoutController implements Initializable {
 
-	//메인앱과 연동 될 객체이다.
-	private MainApp mainApp = null;
-	private BorderPane rootPane = null;
-	
-	@FXML Label titleLabel;	
-	
-	TitleLayoutController controller = null;
-	
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
-		
-	}
+    // 메인앱과 연동 될 객체이다.
+    private MainApp mainApp = null;
+    private BorderPane rootPane = null;
 
-	
-	public RootLayoutController() {
-		// TODO Auto-generated constructor stub
-	}
-	
-	/**
-	 * 타이틀 메뉴를 로드한다.
-	 */
-	private void loadTitleMenu(){
-		try{
-			FXMLLoader loader = new FXMLLoader(RootLayoutController.class.getResource("/PointOfView/View/Title/TitleLayout.fxml"));
-			BorderPane pane = loader.load();
+    @FXML
+    Label titleLabel;
 
-			
-			controller = loader.getController();
-			controller.setMainApp(mainApp);
-			
-			rootPane.setCenter(pane);
-			
-			
-		}catch (Exception e){
-			e.printStackTrace();
-		}
+    TitleLayoutController controller = null;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+	// TODO Auto-generated method stub
+
+    }
+
+    public RootLayoutController() {
+	// TODO Auto-generated constructor stub
+    }
+
+    /**
+     * 타이틀 메뉴를 로드한다.
+     */
+    private void loadTitleMenu() {
+	try {
+	    FXMLLoader loader = new FXMLLoader(
+		    RootLayoutController.class.getResource("/PointOfView/View/Title/TitleLayout.fxml"));
+	    BorderPane pane = loader.load();
+
+	    controller = loader.getController();
+	    controller.setMainApp(mainApp);
+
+	    rootPane.setCenter(pane);
+
+	} catch (Exception e) {
+	    e.printStackTrace();
 	}
-	
-	private void loadTitleLabel() {
-		titleLabel.setText(mainApp.getDataManagement().getPOSTitle());
-	}
-	
-	/**
-	 * RootPane 을 반환한다.
-	 * @return
-	 */
-	public BorderPane getRootPane(){
-		return rootPane;
-	}
-	
-	/**
-	 * 주문 메뉴를 보여준다.
-	 */
-	public void showOrderMenu(){
-		controller.showOrderMenu();
-	}
-	
-	public void showThisPane(Node pane){
-		rootPane.getChildren().clear();
-		
-		rootPane.setCenter(pane);
-	}
-	
-	//MainApp과 연동한다.
-	public void setMainApp(MainApp mainApp, BorderPane rootPane){
-		this.mainApp = mainApp;
-		this.rootPane = rootPane;
-		
-		loadTitleMenu();
-		loadTitleLabel();
-		
-	}
-	
+    }
+
+    private void loadTitleLabel() {
+	titleLabel.setText(mainApp.getDataManagement().getPOSTitle());
+    }
+
+    /**
+     * RootPane 을 반환한다.
+     * 
+     * @return
+     */
+    public BorderPane getRootPane() {
+	return rootPane;
+    }
+
+    /**
+     * 주문 메뉴를 보여준다.
+     */
+    public void showOrderMenu() {
+	controller.showOrderMenu();
+    }
+
+    public void showThisPane(Node pane) {
+	rootPane.getChildren().clear();
+
+	rootPane.setCenter(pane);
+    }
+
+    // MainApp과 연동한다.
+    public void setMainApp(MainApp mainApp, BorderPane rootPane) {
+	this.mainApp = mainApp;
+	this.rootPane = rootPane;
+
+	loadTitleMenu();
+	loadTitleLabel();
+
+    }
+
 }
