1d0fd366f7c4d70bd72174472846ab6fb6ae34b3
Jeongsam <foryou8033j@gmail.com>
2017-10-22 19:26:30 +0900

    소스 정리

--- Point-Of-Sale/src/PointOfView/Models/Stastics/Sale/SaleStasticsModel.java
+++ Point-Of-Sale/src/PointOfView/Models/Stastics/Sale/SaleStasticsModel.java
@@ -12,190 +12,184 @@ import javafx.scene.chart.XYChart;
 
 public class SaleStasticsModel {
 
-	
-	private Receipt receipt;
-	
-	private ObservableList<String> dayNames = FXCollections.observableArrayList();
-	private ObservableList<String> monthNames = FXCollections.observableArrayList();
-	private ObservableList<String> yearNames = FXCollections.observableArrayList();
-	
-	
-	public SaleStasticsModel(Receipt receipt) {
-		this.receipt = receipt;
-		
-		Calendar cal = Calendar.getInstance();
+    private Receipt receipt;
 
-        int lastDayOfPreMonth = cal.getMaximum(Calendar.DAY_OF_MONTH);
-        int today = cal.getTime().getDate();
-        
-        //최근 30일의 날자를 리스트화 한다.
-        for(int i=0; i<30; i++) {
-        	if(today > 0)
-        		dayNames.add(String.valueOf(today--));
-        	else 
-        		dayNames.add(String.valueOf(lastDayOfPreMonth--));
-        }
-        
-        //최근 12달의 월을 리스트화 한다.
-        int curMonth = cal.getTime().getMonth();
-        int lastMonthOfYear = 12;
-        
-        for (int i=0; i<12; i++) {
-        	if(curMonth < 0)
-        		monthNames.add(String.valueOf(curMonth--));
-        	else
-        		monthNames.add(String.valueOf(lastMonthOfYear--));
-        }
-        
-        //최근 10년의 해를 리스트화 한다.
-        int year = cal.get(Calendar.YEAR);
-        
-        for(int i=0; i<10; i++) {
-        	System.out.println(year);
-        	yearNames.add(String.valueOf(year--));
-        }
+    private ObservableList<String> dayNames = FXCollections.observableArrayList();
+    private ObservableList<String> monthNames = FXCollections.observableArrayList();
+    private ObservableList<String> yearNames = FXCollections.observableArrayList();
 
-		
+    public SaleStasticsModel(Receipt receipt) {
+	this.receipt = receipt;
+
+	Calendar cal = Calendar.getInstance();
+
+	int lastDayOfPreMonth = cal.getMaximum(Calendar.DAY_OF_MONTH);
+	int today = cal.getTime().getDate();
+
+	// 최근 30일의 날자를 리스트화 한다.
+	for (int i = 0; i < 30; i++) {
+	    if (today > 0)
+		dayNames.add(String.valueOf(today--));
+	    else
+		dayNames.add(String.valueOf(lastDayOfPreMonth--));
 	}
-	
-	public ObservableList<String> getXAxisModel(CHART_CATEGORY category){
-		if(category.equals(CHART_CATEGORY.DAY))
-			return dayNames;
-		
-		else if(category.equals(CHART_CATEGORY.MONTH))
-			return monthNames;
-		
-		else if(category.equals(CHART_CATEGORY.YEAR))
-			return yearNames;
-		
-		return FXCollections.observableArrayList(new String("ERROR"));
-		
+
+	// 최근 12달의 월을 리스트화 한다.
+	int curMonth = cal.getTime().getMonth();
+	int lastMonthOfYear = 12;
+
+	for (int i = 0; i < 12; i++) {
+	    if (curMonth < 0)
+		monthNames.add(String.valueOf(curMonth--));
+	    else
+		monthNames.add(String.valueOf(lastMonthOfYear--));
 	}
-	
-	/**
-	 * 년별 매출 통계를 반환한다.
-	 * @return	XYChart.Series<String, Integer>
-	 */
-	public XYChart.Series<String, Integer> getYearModel(){
-		Map<String, Integer> yearModel = new HashMap<>();
-		
-        // 월별로 합산 매출액 만든다.
-        for (int i = 0; i < receipt.getReceiptList().size(); i++) {
-        	
-        	String date = String.valueOf(receipt.getReceiptList().get(i).getPayTime().get(Calendar.YEAR));
-        	int price = receipt.getReceiptList().get(i).getPayTableData().getSumPrice();
-        	
-        	if(yearModel.get(date) == null)
-        		yearModel.put(date, price);
-        	else
-        		yearModel.put(date, yearModel.get(date) + price);
-        }
-        	
-        XYChart.Series<String, Integer> series = new XYChart.Series<>();
-        
-        // 그래프 Series 에 데이터를 추가한다.
-        for (int i=0; i < yearNames.size(); i++) {
-        	
-        	String date = yearNames.get(i);
-        	
-        	if(yearModel.get(date) == null)
-        		continue;
-        	
-        	int price = yearModel.get(date);
-        	System.out.println(price);
-        	series.getData().add(new XYChart.Data<>(yearNames.get(i), price));
-        	
-        	yearModel.put(date, yearModel.get(date) + price);
-        }
-        
-        return series;
-		
+
+	// 최근 10년의 해를 리스트화 한다.
+	int year = cal.get(Calendar.YEAR);
+
+	for (int i = 0; i < 10; i++) {
+	    System.out.println(year);
+	    yearNames.add(String.valueOf(year--));
 	}
-	
-	
-	/**
-	 * 월별 매출 통계를 반환한다.
-	 * @return	XYChart.Series<String, Integer>
-	 */
-	public XYChart.Series<String, Integer> getMonthModel(){
-		Map<String, Integer> monthModel = new HashMap<>();
-		
-        // 월별로 합산 매출액 만든다.
-        for (int i = 0; i < receipt.getReceiptList().size(); i++) {
-        	
-        	String date = String.valueOf(receipt.getReceiptList().get(i).getPayTime().get(Calendar.MONTH)+1);
-        	int price = receipt.getReceiptList().get(i).getPayTableData().getSumPrice();
-        	
-        	if(monthModel.get(date) == null)
-        		monthModel.put(date, price);
-        	else
-        		monthModel.put(date, monthModel.get(date) + price);
-        }
-        	
-        XYChart.Series<String, Integer> series = new XYChart.Series<>();
-        
-        // 그래프 Series 에 데이터를 추가한다.
-        for (int i=0; i < monthNames.size(); i++) {
-        	
-        	String date = monthNames.get(i);
-        	
-        	if(monthModel.get(date) == null)
-        		continue;
-        	
-        	int price = monthModel.get(date);
-        	System.out.println(price);
-        	series.getData().add(new XYChart.Data<>(monthNames.get(i), price));
-        	
-        	monthModel.put(date, monthModel.get(date) + price);
-        }
-        
-        return series;
-		
+
+    }
+
+    public ObservableList<String> getXAxisModel(CHART_CATEGORY category) {
+	if (category.equals(CHART_CATEGORY.DAY))
+	    return dayNames;
+
+	else if (category.equals(CHART_CATEGORY.MONTH))
+	    return monthNames;
+
+	else if (category.equals(CHART_CATEGORY.YEAR))
+	    return yearNames;
+
+	return FXCollections.observableArrayList(new String("ERROR"));
+
+    }
+
+    /**
+     * 년별 매출 통계를 반환한다.
+     * 
+     * @return XYChart.Series<String, Integer>
+     */
+    public XYChart.Series<String, Integer> getYearModel() {
+	Map<String, Integer> yearModel = new HashMap<>();
+
+	// 월별로 합산 매출액 만든다.
+	for (int i = 0; i < receipt.getReceiptList().size(); i++) {
+
+	    String date = String.valueOf(receipt.getReceiptList().get(i).getPayTime().get(Calendar.YEAR));
+	    int price = receipt.getReceiptList().get(i).getPayTableData().getSumPrice();
+
+	    if (yearModel.get(date) == null)
+		yearModel.put(date, price);
+	    else
+		yearModel.put(date, yearModel.get(date) + price);
 	}
-	
-	
-	/**
-	 * 일별 매출 통계를 반환한다.
-	 * @return	XYChart.Series<String, Integer>
-	 */
-	public XYChart.Series<String, Integer> getDayModel(){
-		Map<String, Integer> dayModel = new HashMap<>();
-		
-        // 일별로 합산 매출액 만든다.
-        for (int i = 0; i < receipt.getReceiptList().size(); i++) {
-        	
-        	String date = String.valueOf(receipt.getReceiptList().get(i).getPayTime().get(Calendar.DATE));
-        	int price = receipt.getReceiptList().get(i).getPayTableData().getSumPrice();
-        	
-        	if(dayModel.get(date) == null)
-        		dayModel.put(date, price);
-        	else
-        		dayModel.put(date, dayModel.get(date) + price);
-        }
-        	
-        XYChart.Series<String, Integer> series = new XYChart.Series<>();
-        
-        // 그래프 Series 에 데이터를 추가한다.
-        for (int i=0; i < dayNames.size(); i++) {
-        	
-        	String date = dayNames.get(i);
-        	
-        	if(dayModel.get(date) == null)
-        		continue;
-        	
-        	int price = dayModel.get(date);
-        	System.out.println(price);
-        	series.getData().add(new XYChart.Data<>(dayNames.get(i), price));
-        	
-        	dayModel.put(date, dayModel.get(date) + price);
-        }
-        
-        return series;
-		
+
+	XYChart.Series<String, Integer> series = new XYChart.Series<>();
+
+	// 그래프 Series 에 데이터를 추가한다.
+	for (int i = 0; i < yearNames.size(); i++) {
+
+	    String date = yearNames.get(i);
+
+	    if (yearModel.get(date) == null)
+		continue;
+
+	    int price = yearModel.get(date);
+	    System.out.println(price);
+	    series.getData().add(new XYChart.Data<>(yearNames.get(i), price));
+
+	    yearModel.put(date, yearModel.get(date) + price);
 	}
-	
-	
-	
-	
-	
+
+	return series;
+
+    }
+
+    /**
+     * 월별 매출 통계를 반환한다.
+     * 
+     * @return XYChart.Series<String, Integer>
+     */
+    public XYChart.Series<String, Integer> getMonthModel() {
+	Map<String, Integer> monthModel = new HashMap<>();
+
+	// 월별로 합산 매출액 만든다.
+	for (int i = 0; i < receipt.getReceiptList().size(); i++) {
+
+	    String date = String.valueOf(receipt.getReceiptList().get(i).getPayTime().get(Calendar.MONTH) + 1);
+	    int price = receipt.getReceiptList().get(i).getPayTableData().getSumPrice();
+
+	    if (monthModel.get(date) == null)
+		monthModel.put(date, price);
+	    else
+		monthModel.put(date, monthModel.get(date) + price);
+	}
+
+	XYChart.Series<String, Integer> series = new XYChart.Series<>();
+
+	// 그래프 Series 에 데이터를 추가한다.
+	for (int i = 0; i < monthNames.size(); i++) {
+
+	    String date = monthNames.get(i);
+
+	    if (monthModel.get(date) == null)
+		continue;
+
+	    int price = monthModel.get(date);
+	    System.out.println(price);
+	    series.getData().add(new XYChart.Data<>(monthNames.get(i), price));
+
+	    monthModel.put(date, monthModel.get(date) + price);
+	}
+
+	return series;
+
+    }
+
+    /**
+     * 일별 매출 통계를 반환한다.
+     * 
+     * @return XYChart.Series<String, Integer>
+     */
+    public XYChart.Series<String, Integer> getDayModel() {
+	Map<String, Integer> dayModel = new HashMap<>();
+
+	// 일별로 합산 매출액 만든다.
+	for (int i = 0; i < receipt.getReceiptList().size(); i++) {
+
+	    String date = String.valueOf(receipt.getReceiptList().get(i).getPayTime().get(Calendar.DATE));
+	    int price = receipt.getReceiptList().get(i).getPayTableData().getSumPrice();
+
+	    if (dayModel.get(date) == null)
+		dayModel.put(date, price);
+	    else
+		dayModel.put(date, dayModel.get(date) + price);
+	}
+
+	XYChart.Series<String, Integer> series = new XYChart.Series<>();
+
+	// 그래프 Series 에 데이터를 추가한다.
+	for (int i = 0; i < dayNames.size(); i++) {
+
+	    String date = dayNames.get(i);
+
+	    if (dayModel.get(date) == null)
+		continue;
+
+	    int price = dayModel.get(date);
+	    System.out.println(price);
+	    series.getData().add(new XYChart.Data<>(dayNames.get(i), price));
+
+	    dayModel.put(date, dayModel.get(date) + price);
+	}
+
+	return series;
+
+    }
+
 }
--- Point-Of-Sale/src/PointOfView/View/Config/ConfigLayoutController.java
+++ Point-Of-Sale/src/PointOfView/View/Config/ConfigLayoutController.java
@@ -16,6 +16,7 @@ import javafx.scene.text.Text;
 
 /**
  * 환경 설정 레이아웃 컨트롤러
+ * 
  * @author Jeongsam
  *
  */
@@ -35,132 +36,130 @@ public class ConfigLayoutController {
 
     @FXML
     private Text lbnNotice;
-    
+
     private boolean oldPassPass = false;
     private boolean passwordChanged = false;
 
     @FXML
     void handleBackToTitle(ActionEvent event) {
-    	mainApp.showTitleMenu();
+	mainApp.showTitleMenu();
     }
-    
 
     /**
      * 변경 된 데이터를 저장한다.
+     * 
      * @param event
      */
     @FXML
     void handleSave(ActionEvent event) {
-    	
-    	ProcessService service = new ProcessService(new InnerService() {
-    			
-    		@Override
-    		public void doSomeThing(){
-    			InnerService.super.doSomeThing();
-    			try {
-        			Thread.sleep(2000);
-    			}catch (Exception e) {
-    				
-    			}
-    		}
-    		
-		});
-    	
-    	service.setOnSucceeded(e -> {
-    		lbnNotice.setVisible(false);
-    		password.setDisable(false);
-    		repassword.setDisable(false);
-    		service.reset();
-    	});
-    	
-    	if(!name.getText().equals("")) {
 
-    		mainApp.getDataManagement().setPOSTitle(name.getText());
-    	}
-    	
-    	if(!password.getText().equals(repassword.getText())) {
-    		password.setText("");
-    		repassword.setText("");
-    		lbnPasswordAlert.setVisible(false);
-    	}
-    	
-    	if(passwordChanged) {
-    		mainApp.getDataManagement().setAdminPassword(repassword.getText());
-    	}
-    	else {
-    		password.setDisable(true);
-    		repassword.setDisable(true);
-    	}
-    		
-    		
-    	
-    	lbnNotice.setVisible(true);
-    	service.start();
-    	
-    	
+	ProcessService service = new ProcessService(new InnerService() {
+
+	    @Override
+	    public void doSomeThing() {
+		InnerService.super.doSomeThing();
+		try {
+		    Thread.sleep(2000);
+		} catch (Exception e) {
+
+		}
+	    }
+
+	});
+
+	service.setOnSucceeded(e -> {
+	    lbnNotice.setVisible(false);
+	    password.setDisable(false);
+	    repassword.setDisable(false);
+	    service.reset();
+	});
+
+	if (!name.getText().equals("")) {
+
+	    mainApp.getDataManagement().setPOSTitle(name.getText());
+	}
+
+	if (!password.getText().equals(repassword.getText())) {
+	    password.setText("");
+	    repassword.setText("");
+	    lbnPasswordAlert.setVisible(false);
+	}
+
+	if (passwordChanged) {
+	    mainApp.getDataManagement().setAdminPassword(repassword.getText());
+	} else {
+	    password.setDisable(true);
+	    repassword.setDisable(true);
+	}
+
+	lbnNotice.setVisible(true);
+	service.start();
+
     }
-    
+
     /**
      * 패스워드 입력 TextField 의 무결성을 점검한다.
+     * 
      * @param event
      */
     @FXML
     void handlePasswordFieldInputDialog(MouseEvent event) {
-    	
-    	if(!oldPassPass) {
-    		if(new PasswordInputDialog(mainApp).isPass())
-    			oldPassPass = true;
-    	}
-    	
-    	if(!oldPassPass)
-    		return;
-    	
-    	PasswordInputDialog dialog = new PasswordInputDialog(mainApp, Mode.MODIFY);
-    	dialog.setMode(Mode.MODIFY);
-    	dialog.showAndWait();
-    	password.setText(dialog.getValue());
+
+	if (!oldPassPass) {
+	    if (new PasswordInputDialog(mainApp).isPass())
+		oldPassPass = true;
+	}
+
+	if (!oldPassPass)
+	    return;
+
+	PasswordInputDialog dialog = new PasswordInputDialog(mainApp, Mode.MODIFY);
+	dialog.setMode(Mode.MODIFY);
+	dialog.showAndWait();
+	password.setText(dialog.getValue());
     }
 
     /**
      * 패스워드 재입력 TextField의 무결성을 점검한다.
+     * 
      * @param event
      */
     @FXML
     void handleRePasswordFieldInputDialog(MouseEvent event) {
-    	PasswordInputDialog dialog = new PasswordInputDialog(mainApp, Mode.MODIFY);
-    	dialog.setMode(Mode.MODIFY);
-    	dialog.showAndWait();
-    	repassword.setText(dialog.getValue());
-    	
-    	if(password.getText().equals(repassword.getText())) {
-    		passwordChanged = true;
-			lbnPasswordAlert.setVisible(false);
-    	}
+	PasswordInputDialog dialog = new PasswordInputDialog(mainApp, Mode.MODIFY);
+	dialog.setMode(Mode.MODIFY);
+	dialog.showAndWait();
+	repassword.setText(dialog.getValue());
 
-    	else {
-		lbnPasswordAlert.setText("패스워드가 일치하지 않습니다.");
-		lbnPasswordAlert.setVisible(true);
-		passwordChanged = false;
-    	}
+	if (password.getText().equals(repassword.getText())) {
+	    passwordChanged = true;
+	    lbnPasswordAlert.setVisible(false);
+	}
+
+	else {
+	    lbnPasswordAlert.setText("패스워드가 일치하지 않습니다.");
+	    lbnPasswordAlert.setVisible(true);
+	    passwordChanged = false;
+	}
     }
 
-    
     private MainApp mainApp;
-    
+
     /**
      * MainApp과 연동한다.
+     * 
      * @param mainApp
      */
     public void setMainApp(MainApp mainApp) {
-    	this.mainApp = mainApp;
-    	lbnNotice.setVisible(false);
-    	lbnPasswordAlert.setVisible(false);
-    	
-    	// 기존의 데이터 반영
-    	password.setPromptText("패스워드 입력");
-    	repassword.setPromptText("패스워드 재입력");
-    	
-    	name.setText(mainApp.getDataManagement().getPOSTitle());
+	this.mainApp = mainApp;
+	lbnNotice.setVisible(false);
+	lbnPasswordAlert.setVisible(false);
+
+	// 기존의 데이터 반영
+	password.setPromptText("패스워드 입력");
+	repassword.setPromptText("패스워드 재입력");
+
+	name.setText(mainApp.getDataManagement().getPOSTitle());
 
     }
 
